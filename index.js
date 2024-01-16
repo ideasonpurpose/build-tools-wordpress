c17651fcb096fed802db225e9887ca473f1f0c34
Joe Maller <joe@joemaller.com>
2024-01-16 13:43:28 -0500

    log exports

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ import DependencyManifestPlugin from "./lib/DependencyManifestPlugin.js";
 import devserverProxy from "./lib/devserver-proxy.js";
 import WatchRunReporterPlugin from "./lib/WatchRunReporterPlugin.js";
 
+
+console.log({
+  AfterDoneReporterPlugin,
+  buildConfig,
+  DependencyManifestPlugin,
+  devserverProxy,
+  WatchRunReporterPlugin,
+})
+
 export default {
   AfterDoneReporterPlugin,
   buildConfig,
