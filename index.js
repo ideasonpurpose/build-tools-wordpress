63e0a49407c0fcb67506ade1b8b7fbc65e4f37ed
Joe Maller <joe@joemaller.com>
2024-01-16 13:33:04 -0500

    buildConfig

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import AfterDoneReporterPlugin from "./lib/AfterDoneReporterPlugin.js";
+import buildConfig from "./lib/buildConfig.js";
 import DependencyManifestPlugin from "./lib/DependencyManifestPlugin.js";
 import devserverProxy from "./lib/devserver-proxy.js";
 import WatchRunReporterPlugin from "./lib/WatchRunReporterPlugin.js";
 
 export default {
   AfterDoneReporterPlugin,
+  buildConfig,
   DependencyManifestPlugin,
   devserverProxy,
   WatchRunReporterPlugin,
