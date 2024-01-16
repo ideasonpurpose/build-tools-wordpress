d880588240d52dfe7ed44a819647865890efdfb7
Joe Maller <joe@joemaller.com>
2024-01-16 13:22:10 -0500

    add lib, temp exports

---
+++ index.js
@@ -0,0 +1,11 @@
+import AfterDoneReporterPlugin from "./lib/AfterDoneReporterPlugin.js";
+import DependencyManifestPlugin from "./lib/DependencyManifestPlugin.js";
+import devserverProxy from "./lib/devserver-proxy.js";
+import WatchRunReporterPlugin from "./lib/WatchRunReporterPlugin.js";
+
+export default {
+  AfterDoneReporterPlugin,
+  DependencyManifestPlugin,
+  devserverProxy,
+  WatchRunReporterPlugin,
+};
