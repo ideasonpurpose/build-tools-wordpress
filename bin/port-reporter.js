767c12c58689820cce5afda4eec109bbb590851f
Joe Maller <joe@joemaller.com>
2024-01-24 22:05:21 -0500

    reconfigure imports, port-reporter to module, lots of deps

--- bin/port-reporter.js
+++ bin/port-reporter.js
@@ -1,22 +1,40 @@
 #! /usr/bin/env node
-import { exec } from "node:child_process";
 
 import chalk from "chalk";
 
-exec("docker compose port wordpress 80", (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
-  if (stdout) {
-    const [addr, port] = stdout.split(":");
+import { findLocalPort } from "../index.js";
 
-    console.log("\n",
+/**
+ * This is a reporting/display wrapper for the local port discovery module
+ */
+const main = async () => {
+  const info = await findLocalPort().catch((error) => ({ error }));
+
+  if (info && info.port && info.hostname) {
+    console.log(
+      "\n",
       " 🚀 ",
-     chalk.bold( "Local WP site:"),
-      chalk.magenta(`http://localhost:${chalk.bold(port)}`),
+      chalk.bold("Local WP site:"),
+      chalk.magenta(`http://${info.hostname}:${chalk.bold(info.port)}`),
+      "\n",
     );
+  } else {
+    let errString, showDebug;
+
+    if (info.error) {
+      errString = info.error.toString();
+    } else {
+      errString = "Unknown error occurred. Unable to discover local port";
+      showDebug = true;
+    }
+
+    errString = errString.split("\n").join("\n      ");
+
+    console.log("\n", " 🤖 ", chalk.red.bold(errString));
+    if (showDebug) {
+      console.log(chalk.green("  🐛  Debug:"), info);
+    }
   }
-  // console.log(`stdout: ${stdout}`);
-  // console.error(`stderr: ${stderr}`);
-});
+};
+
+main();
--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-import AfterDoneReporterPlugin from "./lib/AfterDoneReporterPlugin.js";
-import buildConfig from "./lib/buildConfig.js";
-import DependencyManifestPlugin from "./lib/DependencyManifestPlugin.js";
-import devserverProxy from "./lib/devserver-proxy.js";
-import WatchRunReporterPlugin from "./lib/WatchRunReporterPlugin.js";
+export { AfterDoneReporterPlugin } from "./lib/AfterDoneReporterPlugin.js";
+export { buildConfig } from "./lib/buildConfig.js";
+export { DependencyManifestPlugin } from "./lib/DependencyManifestPlugin.js";
+// export *  from "./lib/DependencyManifestPlugin.js";
+export { devserverProxy } from "./lib/devserver-proxy.js";
+export { WatchRunReporterPlugin } from "./lib/WatchRunReporterPlugin.js";
+export { findLocalPort } from "./lib/find-local-docker-port.js";
+// export * from  "./lib/find-local-docker-port.js";
+// import findLocalPort from "./lib/find-local-docker-port.js";
 
-
-
-// export { };
-export  {
-  AfterDoneReporterPlugin,
-  buildConfig,
-  DependencyManifestPlugin,
-  devserverProxy,
-  WatchRunReporterPlugin,
-};
+// export default {
+//   // AfterDoneReporterPlugin,
+//   //   buildConfig,
+//   // DependencyManifestPlugin,
+//   //   devserverProxy,
+//   //   WatchRunReporterPlugin,
+//   //   findLocalPort,
+//   //   // moo: () => console.log("COW SAYS"),
+// };
+// export const moo = () => console.log("COW SAYS");
